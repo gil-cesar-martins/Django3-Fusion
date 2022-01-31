93ae631fea76099fe4ce8c980b68ee3495c20745
gil-cesar-martins <gilcesartst@gmail.com>
2022-01-31 10:28:17 -0300

    admin lte2

--- fusion/settings.py
+++ fusion/settings.py
@@ -23,13 +23,17 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
+    
+    'django_adminlte',
+    'django_adminlte_theme',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'core',
+    'django.contrib.staticfiles', 
 ]
 
 MIDDLEWARE = [
