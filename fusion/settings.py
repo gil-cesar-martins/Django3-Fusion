10ee07476a67a4075a2e4b52e7162629eb92f7e4
gil-cesar-martins <gilcesartst@gmail.com>
2022-02-04 13:17:36 -0300

    Atualização2.1

--- fusion/settings.py
+++ fusion/settings.py
@@ -82,7 +82,6 @@ DATABASES = {
     }
 }
 
-
 """
 DATABASES ={
     'default': dj_database_url.config()
@@ -131,6 +130,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Email teste console
 EMAIL_BACKEND ='django.core.mail.backends.console.EmailBackend'
